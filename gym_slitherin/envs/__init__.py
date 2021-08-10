5e9244e1faa2223d97fbe4afdb25ce49783bba3b
georgestamatelis <sdi1800185@Gmail.com>
2021-08-10 13:25:31 +0300

    First car parking level is ok

--- gym_slitherin/envs/__init__.py
+++ gym_slitherin/envs/__init__.py
@@ -4,4 +4,5 @@ from gym_slitherin.envs.hill_climber_env import Hill_Climber_Env
 
 from gym_slitherin.envs.carParking import CarParking
 from gym_slitherin.envs.carParking2 import carParking2
+from gym_slitherin.envs.carParking3 import CarParking3
 
